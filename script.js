dd98a20dc5a3294a4460d08233c1b0a9f660b255
Isa Dutra <134721144+isadtr@users.noreply.github.com>
2024-06-06 09:54:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+const texts = ['Bem-vindo à Nossa Landing Page!', 'Oferecemos os melhores serviços!', 'Entre em contato conosco!'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type() {
+  if (count === texts.length) {
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector('#typing-effect').textContent = letter;
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+    setTimeout(type, 2000); // Pausa de 2 segundos antes de começar a próxima frase
+  } else {
+    setTimeout(type, 100); // Velocidade da digitação
+  }
+})();
